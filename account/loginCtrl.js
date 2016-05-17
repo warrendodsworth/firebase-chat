667554058a1cf551b2039144a4a5ac66725aa0c7
warrendodsworth <warren.dodsworth@gmail.com>
2016-05-17 11:26:09 +1000

    Add Firebase facebook auth basics

--- account/loginCtrl.js
+++ account/loginCtrl.js
@@ -1,15 +1,25 @@
-(function() {
-'use strict';
+(function () {
+  'use strict';
 
   angular
     .module('app')
-    .controller('loginCtrl', loginCtrl );
+    .controller('loginCtrl', loginCtrl);
 
-  loginCtrl.$inject = ['$scope'];
+  loginCtrl.$inject = ['$scope', '$firebaseAuth'];
 
-  function loginCtrl($scope) {
+  function loginCtrl($scope, $firebaseAuth) {
     var vm = $scope;
 
-    
+    // create an instance of the authentication service
+    var auth = $firebaseAuth(ref);
+
+    // login with Facebook
+    vm.facebookLogin = function () {
+      auth.$authWithOAuthPopup("facebook").then(function (authData) {
+        console.log("Logged in as:", authData.uid);
+      }).catch(function (error) {
+        console.log("Authentication failed:", error);
+      });
+    };
   }
 })();
