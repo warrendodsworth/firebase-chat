95353372e4b8d9ca61b925640a03189671224b80
warrendodsworth <warren.dodsworth@gmail.com>
2016-06-03 12:03:41 +1000

    Add accountService.js

--- www/account/loginCtrl.js
+++ www/account/loginCtrl.js
@@ -5,50 +5,35 @@
     .module('app')
     .controller('loginCtrl', loginCtrl);
 
-  loginCtrl.$inject = ['$scope', '$firebaseAuth', '$location', 'localStorageService'];
+  loginCtrl.$inject = ['$scope', '$firebaseAuth', '$location', 'accountService'];
 
-  function loginCtrl($scope, $firebaseAuth, $location, ls) {
+  function loginCtrl($scope, $firebaseAuth, $location, accountService) {
     var ref = new Firebase('https://dazzling-fire-5094.firebaseio.com');
     var vm = $scope;
-    var isNewUser = true;
 
     vm.facebookLogin = function () {
-      ref.authWithOAuthPopup('facebook', function (authData) {
+      ref.authWithOAuthPopup('facebook', function (error, authData) {
         if (authData) {
           console.log("User " + authData.uid + " is logged in with " + authData.provider);
-          $location.path('/');
+          $scope.$apply(function () {
+            $location.path('/');
+          });  
         } else {
           console.log("User is logged out");
         }
-      });
-    };  
-    
-    ref.onAuth(function (authData) {
-      console.log(authData);
-      if (authData && isNewUser) {
-        // save the user's profile into the database so we can list users,
-        // use them in Security and Firebase Rules, and show profiles
-        ref.child("users").child(authData.uid).set({
-          provider: authData.provider,
-          name: getName(authData)
+      }, {
+          remember: "default", //sessionOnly
+          scope: "email,user_likes"
         });
-      }
-    });
-
-    //Helper
-    function getName(authData) {
-      switch (authData.provider) {
-        case 'password':
-          return authData.password.email.replace(/@.*/, '');
-        case 'twitter':
-          return authData.twitter.displayName;
-        case 'facebook':
-          return authData.facebook.displayName;
-      }
-    }
-
-  }
+    };
+  } 
 })();
+
+
+
+
+
+
 
 // https://www.firebase.com/docs/web/libraries/angular/guide/intro-to-angularfire.html#section-angularfire-intro
 // var auth = $firebaseAuth(ref);
--- www/app.js
+++ www/app.js
@@ -1,29 +1,30 @@
 (function () {
-    'use-strict';
-    angular.module('app', [
-        'ngRoute',
-        'firebase',
-        'LocalStorageModule'
-    ]);
-    angular.module('app').config([
-      '$routeProvider',
-      function ($routeProvider) {
-        $routeProvider.when('/', {
-          templateUrl: 'home/home.html',
-          controller: 'homeCtrl'
-        }).when('/chat/:id', {
-          templateUrl: 'home/chat.html',
-          controller: 'chatCtrl'
-        }).when('/login', {
-          templateUrl: 'account/login.html',
-          controller: 'loginCtrl'
-        }).otherwise('/login');
-      }
-    ]).config([
-      '$httpProvider',
-      function ($httpProvider) {
-        $httpProvider.interceptors.push('HttpInterceptorService');
-      }
-    ]);
-    angular.module('app').constant('globals', {});
-}());
+  'use-strict';
+  angular.module('app', [
+    'ngRoute',
+    'firebase',
+    'ui.bootstrap',
+    'LocalStorageModule'
+  ]);
+  angular.module('app').config([
+    '$routeProvider',
+    function ($routeProvider) {
+      $routeProvider.when('/', {
+        templateUrl: 'home/home.html',
+        controller: 'homeCtrl'
+      }).when('/chat/:id', {
+        templateUrl: 'home/chat.html',
+        controller: 'chatCtrl'
+      }).when('/login', {
+        templateUrl: 'account/login.html',
+        controller: 'loginCtrl'
+      }).otherwise('/login');
+    }
+  ]).config([
+    '$httpProvider',
+    function ($httpProvider) {
+      $httpProvider.interceptors.push('HttpInterceptorService');
+    }
+  ]);
+  angular.module('app').constant('globals', {});
+} ());
