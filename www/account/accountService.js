b159cf64e842e67a5845e77a9207c5711ce073fd
warrendodsworth <warren.dodsworth@gmail.com>
2016-06-07 12:15:43 +1000

    add identity to accSvc

--- www/account/accountService.js
+++ www/account/accountService.js
@@ -11,7 +11,7 @@
     var ref = new Firebase('https://dazzling-fire-5094.firebaseio.com');
     var service = {};
 
-    $rootScope.identity = { auth: false };
+    service.identity = $rootScope.identity = { auth: false };
 
     service.logout = function () {
       ref.unauth();
@@ -34,18 +34,18 @@
             });
           }
         });
-  
+
         //User authData to set identity
         $rootScope.identity = {
           auth: true,
           name: getName(authData),
           provider: authData.provider
         };
-  
+
         console.log('svc: onauth fired');
         console.log(authData);
-        
-      }  else{
+
+      } else {
         console.log('svc: not logged in');
       }
     });
@@ -80,6 +80,6 @@
 //     uid:"facebook:10154384221665145"
 //   },
 //   expires: 1465004038,
-//   provider: 'facebook', token:"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IndhcnJlbi5kb2Rzd29ydGhAZ21haWwuY29tIiwicHJvdmlkZXJfaWQiOiIxMDE1NDM4NDIyMTY2NTE0NSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiaWF0IjoxNDY0OTE3NjM4LCJ2IjowLCJkIjp7InVpZCI6ImZhY2Vib29rOjEwMTU0Mzg0MjIxNjY1MTQ1IiwicHJvdmlkZXIiOiJmYWNlYm9vayJ9fQ.prqWBtoigt_jzq08b_pcygo2hKnolXmlIXE8EQgPzLo",
+//   provider: 'facebook', token:"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9",
 //   uid:"facebook:10154384221665145"
 // }
