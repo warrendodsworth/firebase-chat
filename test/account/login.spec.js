9c76f74658f5d3fc262835f355b135a86df3ccb7
warrendodsworth <warren.dodsworth@gmail.com>
2016-06-29 10:44:24 +1000

    update angular mocks; add gulp-protractor

--- test/account/login.spec.js
+++ test/account/login.spec.js
@@ -1,6 +1,10 @@
 /// <reference path="../../typings/index.d.ts" /> 
 
 describe('account.login', function () {
+  beforeEach(function () {
+    browser().navigateTo('/#/login');
+  })
+
   it('should login with a test account', function() {
     expect(true).toBe(true);
   });
