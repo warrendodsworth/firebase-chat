4774a4b311ed6b4dc6d96504ac321f1244597ec3
warrendodsworth <warren.dodsworth@gmail.com>
2016-06-30 15:10:39 +1000

    add basic angular service test

--- test/labs/calculator.spec.js
+++ test/labs/calculator.spec.js
@@ -4,9 +4,11 @@ describe('calculator', function () {
   beforeEach(module('calculatorApp'));
 
   var $controller;
-
-  beforeEach(inject(function (_$controller_) {
+  var service;
+  beforeEach(inject(function (_$controller_, CalculateService) {
     $controller = _$controller_;
+
+    service = CalculateService;
   }));
 
   describe('defaults', function () {
@@ -17,13 +19,25 @@ describe('calculator', function () {
     });
   });
 
-  describe('sum', function () {
+  describe('operations', function () {
     it('1 + 1 should equal 2', function () {
       var $scope = {};
       var controller = $controller('CalculatorController', { $scope: $scope });
       $scope.sum(1, 2);
       expect($scope.z).toBe(3);
     });
+
+    //test service
+    it('1 * 1 should equal 1', function () {
+      var result = service.multiply(2, 4);
+      expect(result).toBe(8);
+    });
   });
 
 });
+
+
+// Get the service from the injector - short used above
+// angular.mock.inject(function GetDependencies(CalculateService) {
+//   service = CalculateService;
+// });
--- www/labs/calculator.app.js
+++ www/labs/calculator.app.js
@@ -1,11 +1,21 @@
 (function () {
-  angular.module('calculatorApp', [])
+  angular
+    .module('calculatorApp', [])
+    .service('CalculateService', function CalculateService() {
+      var service = {};
+
+      service.multiply = function (a, b) {
+        return a * b;
+      }
+
+      return service;
+    })
     .controller('CalculatorController', function CalculatorController($scope) {
       $scope.z = 0;
 
       $scope.sum = function (a, b) {
         $scope.z = a + b;
       };
-      
+
     });
 })();
