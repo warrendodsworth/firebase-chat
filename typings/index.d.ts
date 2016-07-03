fe1fda3a8f8e781eed9eb4c2be297ba9f6ff550a
warrendodsworth <warren.dodsworth@gmail.com>
2016-07-04 09:47:18 +1000

    add typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,7 @@
+/// <reference path="globals/angular-protractor/index.d.ts" />
 /// <reference path="globals/angular/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/moment/index.d.ts" />
+/// <reference path="globals/q/index.d.ts" />
 /// <reference path="modules/debug/index.d.ts" />
