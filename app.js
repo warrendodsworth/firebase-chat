1ff84cc4a9a3638ad3d288922ab322bbac257e02
warrendodsworth <warren.dodsworth@gmail.com>
2016-07-05 12:34:59 +1000

    add launch.json to debug node server code

---
+++ app.js
@@ -0,0 +1,15 @@
+//Node server
+var firebase = require('firebase');
+var config = {
+  apiKey: 'AIzaSyC9xO8omc7TxZZ0n4SOQW3bpE-uRryaVD4',
+  authDomain: 'dazzling-fire-5094.firebaseapp.com',
+  databaseURL: 'https://dazzling-fire-5094.firebaseio.com/',
+  storageBucket: 'gs://dazzling-fire-5094.appspot.com'
+};
+firebase.initializeApp(config);
+
+var db = firebase.database();
+var ref = db.ref("users");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
--- server/server.js
+++
@@ -1,1 +0,0 @@
-var firebase = require('firebase');
